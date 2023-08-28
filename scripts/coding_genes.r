facded2e74fdff245398d52d35ed7de605f5f7ce
sahuno <ekwame001@gmail.com>
2023-08-27 23:51:15 -0400

    scripts finalized

--- scripts/coding_genes.r
+++ scripts/coding_genes.r
@@ -1,3 +1,4 @@
+#name: samuel ahuno
 #load packages
 library(DESeq2) #main desdeq analysis
 library(tidyverse)
@@ -29,7 +30,7 @@ dir.create(paste0("data/"), recursive = TRUE, showWarnings = TRUE)
 
 
 blind_transform <- FALSE #should the rlog transformation be blind
-
+drop_samples <- c("veh_2") #samples to drop from analysis
 ########################################################################################
 ## #read in data, extract coding genes counts 
 ########################################################################################
@@ -74,15 +75,30 @@ metadata_df <- read.delim('data/sample_metadata.tsv', header = TRUE)
 
 #Assign new sample names
 cond_assign_new_sample_names = ncol(metadata_df) > 2 & any(str_detect(names(metadata_df), "new_samples_name"))
+
 if(cond_assign_new_sample_names){
+
+  #drop samples
+if(!is.null(drop_samples)){
+metadata_df <- metadata_df %>% dplyr::filter(!samples %in% drop_samples)
+cts_coding <- cts_coding %>% dplyr::select(!all_of(drop_samples))
+}
+
+#assign new sample names
 idx_samples <- match(metadata_df$samples, colnames(cts_coding)) #check if samples match
 names(cts_coding) <- metadata_df[,"new_samples_name"][idx_samples]
 
 metadata_rown_df <- metadata_df %>% column_to_rownames("new_samples_name")
 
-}
+}else{
+#drop samples
+# if(!is.null(drop_samples)){
+# metadata_df <- metadata_df %>% dplyr::filter(!samples %in% drop_samples)
+# }
 
 metadata_rown_df <- metadata_df %>% column_to_rownames("samples")
+}
+
 
 
 ##exclude samples from dseq analysis
@@ -156,15 +172,12 @@ pltPCA_samples <- plotPCA(rld,
 ggsave(pltPCA_samples, file = paste0("figures/PCA_by_condition_and_samples.pdf"), width = 12, height = 12)
 
 
-
-
-
 # Extract the rlog matrix from the object and compute pairwise correlation values
 rld_mat <- assay(rld)
 rld_cor <- cor(rld_mat)
 
 #save a copy of the rlog matrix
-fwrite(rld_mat, file=paste0("data/DESeq2_rlog_Transform_Blind",blind_transform,".tsv"), sep="\t")
+fwrite(rld_mat %>% data.frame(), file=paste0("data/DESeq2_rlog_Transform_Blind",blind_transform,".tsv"), sep="\t")
 
 
 
@@ -179,19 +192,59 @@ res_rld_mat_df <- rld_mat %>%
   as_tibble()
 
 res_rld_mat_df_pivtLonger <- res_rld_mat_df %>% pivot_longer(!c("gene.id"), names_to = "samples", values_to = "rlog_transformed_counts")
-head(res_rld_mat_df_pivtLonger)
+#head(res_rld_mat_df_pivtLonger)
 
+#slotNames(dds)
+# dds_copy <- dds
+# # colnames(dds_copy)
+# colDat <- colData(dds_copy) %>% as.data.frame()
+# idx_samples <- colDat %>% dplyr::filter(condition %in% contrast_as_vect) %>% rownames_to_column("samples.id") %>% pull("samples.id")
+# #idx_samples <- c(idx_samples, "Ctrl_4")
+# dds_copy_subset <- dds_copy[,idx_samples]
+# dds_copy_subset <- DESeq(dds_copy_subset)
+# contrast_input <- "PARPi_vs_CTRL" #uncomment to test run function
+
+# metadata(dds_copy)
+# elementMetadata(dds_copy)
+# dseqObject <- dds_copy[,-idx]
 
 #####################################################################################################################
 ############################### Step 3; Run deseq2 analysis for each contrast
 #####################################################################################################################
 DEResults_ls <- list()
-run_multiple_contrasts <- function(contrast_input, dseqObject = dds, shrink = FALSE, padj_val = 0.05){
+run_multiple_contrasts <- function(contrast_input, dseqObject = dds, shrink = FALSE, padj_val = 0.05, export_transformed_counts = FALSE){
+
+dir.create(paste0("data/", contrast_input, "/"), recursive = TRUE, showWarnings = TRUE)
+dir.create(paste0("figures/", contrast_input, "/"), recursive = TRUE, showWarnings = TRUE)
 
 #contrast_label <- paste(contrast_input, collapse = "_") #used for plot labels
 str_vec <- gsub("_vs_", "_", contrast_input); 
 contrast_as_vect <- unlist(str_split(str_vec, pattern="_"))
 
+# dseqObject <- dseqObject[,-idx]
+
+
+# export Transform counts for data visualization
+if(export_transformed_counts){
+dseqObject_copy <- dseqObject
+colDat <- colData(dseqObject_copy) %>% as.data.frame()
+idx_samples <- colDat %>% dplyr::filter(condition %in% contrast_as_vect) %>% rownames_to_column("samples.id") %>% pull("samples.id")
+dds_copy_subset <- dseqObject_copy[,idx_samples]
+dds_copy_subset <- DESeq(dds_copy_subset)
+
+
+rld <- rlog(dds_copy_subset, #export data
+	    blind=TRUE)
+
+rld_mat <- assay(rld)
+rld_df <- rld_mat %>% as.data.frame() %>% rownames_to_column("gene.id")
+# head(rld_df)
+#save a copy of the rlog matrix
+write_tsv(rld_df, file=paste0("data/",contrast_input,"/DESeq2_rlog_Transform_BlindTRUE_",contrast_input,".tsv"))
+}
+
+# dt_t <- fread("/juno/work/greenbaum/users/ahunos/apps/RNA-seq_DiffExpr/data/condition_PARPi_vs_CTRL/DESeq2_rlog_Transform_BlindTRUE.tsv")
+# head(dt_t)
 #run DESeq analysis - normalization and filtering
 
 results_per_contrast <- results(dseqObject, alpha=padj_val, contrast=contrast_as_vect) #use alpha of 0.05
@@ -206,8 +259,7 @@ res <- lfcShrink(dds,
      # The coef will be dependent on what your contras was. and should be identical to what
 }
 
-dir.create(paste0("data/", contrast_input, "/"), recursive = TRUE, showWarnings = TRUE)
-dir.create(paste0("figures/", contrast_input, "/"), recursive = TRUE, showWarnings = TRUE)
+
 
 # pdf("MA_plot_lfcShrink_dmso_vrs_all_treated.pdf")
 # plotMA(res, ylim=c(-2,2))
@@ -277,7 +329,7 @@ ggsave(heatmap_DE_sig_plot, file = paste0("figures/",contrast_input,"/Heatmap_si
 
 
 sig_genes_ids_merged_with_rlog <- sigLRT_genes %>% dplyr::select(c(gene.symbol, gene.id)) %>% 
-                                            inner_join(res_rld_mat_df, by="gene.id") #%>% 
+                                            left_join(res_rld_mat_df, by="gene.id") #%>% 
                                                                         #dplyr::select(gene.id)
 #head(sig_genes_ids_merged_with_rlog)
 rlog_4Clustering <- sig_genes_ids_merged_with_rlog %>% dplyr::select(!c(gene.id))
@@ -288,7 +340,7 @@ rlog_4Clustering_matrix <- data.matrix(rlog_4Clustering)
 annot_coldf <- as.data.frame(colData(dds)[,"condition", drop=FALSE])
 
 
-pdf(file = paste0("figures/",contrast_input,"/Clustering_sig_DE",contrast_input,"_padjust",padj_val,".pdf"))
+pdf(file = paste0("figures/",contrast_input,"/Clustering_sig_DE",contrast_input,"_padjust",padj_val,".pdf"), width=12, height=9)
 pheatmap(rlog_4Clustering_matrix, main = paste0("Clustering of Sig DGE ", contrast_input), annotation_col=annot_coldf)
 dev.off()
 
@@ -301,10 +353,10 @@ return(results_per_contrast) #return results for specific contrasts
 ############################### Step #; Run DE for each contrast
 ##############################################################################################
 #run for all contrasts
-DEResults_ls <- lapply(contrasts_ls[1], function(x) run_multiple_contrasts(contrast_input=x, dseqObject = dds, shrink = FALSE))
-names(DEResults_ls) <- contrasts_ls[1]
+DEResults_ls <- lapply(contrasts_ls, function(x) run_multiple_contrasts(contrast_input=x, dseqObject = dds, shrink = FALSE, export_transformed_counts = TRUE))
+names(DEResults_ls) <- contrasts_ls
 #summary(DEResults_ls[[1]])
-
+# data.frame(DEResults_ls[[1]]) %>% dplyr::filter(padj < 0.05) %>% dim()
 
 
 
@@ -318,13 +370,13 @@ names(DEResults_ls) <- contrasts_ls[1]
 ################################################################################################
 ############################### Step #; GSEA
 ##############################################################################################
-gsea_analysis <- function(name_de_res, specie_type = "Homo sapiens", category_tag = "H", nCategory_2show = 10, nShowBar=100,fractEnriched=0.5,
-                            ggwidth = 9, ggheight = 7){
+gsea_analysis <- function(name_de_res, specie_type = "Homo sapiens", category_tag = "H", nCategory_2show = 10, nShowBar=100,
+                          fractEnriched=0.5, do_gse_GO = TRUE, do_GSEA=TRUE, ggwidth = 9, ggheight = 7){
   require(DOSE)
   library(msigdbr)
 
 # name_de_res="condition_PARP_CTRL"
-dir.create(paste0("figures/", name_de_res,"/enrichment_Plots/"), recursive = TRUE, showWarnings = TRUE)
+dir.create(paste0("figures/", name_de_res,"/enrichment_Plots/GO/"), recursive = TRUE, showWarnings = TRUE)
 
 
 
@@ -346,8 +398,9 @@ names(foldchanges) <- res_entrez$entrez.gene.id
 ## Sort fold changes in decreasing order
 foldchanges <- sort(foldchanges, decreasing = TRUE)
 
-#do gene set enrichment using GO
-gse <- gseGO(geneList=foldchanges, 
+if(do_gse_GO){
+    #do gene set enrichment using GO
+gse <- gseGO(geneList = foldchanges, 
              ont ="ALL", 
              keyType = "ENTREZID", 
               eps = 0,
@@ -366,7 +419,7 @@ ggsave(dotplt_gse_GO_out, file = paste0("figures/",name_de_res,"/dotplot_gse_GO"
 ## Output images for all significant KEGG pathways
 func_gse_GO_plot2  <- function(x) {
 gse_GO_plot2 <-  enrichplot::gseaplot2(gse, geneSetID=gse$ID[x], title = gse$ID[x])
-ggsave(gse_GO_plot2, file = paste0("figures/",name_de_res,"/enrichment_Plots/gseaPlot_gse_GO","_",gse$ID[x],"_" ,name_de_res,".pdf"))
+ggsave(gse_GO_plot2, file = paste0("figures/",name_de_res,"/enrichment_Plots/GO/gseaPlot_gse_GO","_",gse$ID[x],"_" ,name_de_res,".pdf"))
 
 }
 purrr::map(1:length(gse$ID), func_gse_GO_plot2)
@@ -374,6 +427,11 @@ purrr::map(1:length(gse$ID), func_gse_GO_plot2)
 
 message("extracting GSE results table from gseGO object")
 gse_results <- gse@result
+
+write.table(gse_results, 
+file=paste0("data/",name_de_res,"/GSE_GO_enrichedSets.tsv"),
+sep="\t", row.names = FALSE, col.names = TRUE)
+
 #rank gene sets by normalized  enrichment scores and q-values
 gse_results_sig_qval <- gse_results %>% dplyr::filter(qvalues < 0.05) %>% arrange(desc(abs(NES))) #%>% head() #sigGSEA
 
@@ -388,11 +446,15 @@ coord_flip() +
    geom_vline(xintercept = 0, color = "blue") +
     theme(plot.title = element_text(hjust = -1))
 
-ggsave(lollipop_gse, file = paste0("figures/lollipop_gse","_",name_de_res,"_top_frac_",nCategory_2show,".pdf"), width = ggwidth, height = ggheight)
+ggsave(lollipop_gse, file = paste0("figures/",name_de_res,"/lollipop_GSE_Go_top_",fractEnriched,".pdf"), width = ggwidth, height = ggheight)
+
+}
 
 
+if(do_GSEA){
+    message("performing GSEA using MSigDb ", category_tag," Specie - ", specie_type)
+    dir.create(paste0("figures/", name_de_res,"/enrichment_Plots/GSEA_MSigDb_",category_tag,"/"), recursive = TRUE, showWarnings = TRUE)
 
-message("performing GSEA using MSigDb ", category_tag," Specie - ", specie_type)
 #get data base to use
 m_t2g <- msigdbr(species = specie_type, category = category_tag) %>% 
   dplyr::select(gs_name, entrez_gene) #hall mark genes
@@ -413,7 +475,7 @@ get_gsea_plot2  <- function(x) {
 #    pathview(gene.data = foldchanges, pathway.id = gseaKEGG_results$ID[x], species = "hsa", 
 #        limit = list(gene = 2, cpd = 1))
 gsea_plot2 <-  enrichplot::gseaplot2(msig_GSEA_obj, geneSetID=msig_GSEA_obj$ID[x], title = msig_GSEA_obj$ID[x])
-ggsave(gsea_plot2, file = paste0("figures/",name_de_res,"/enrichment_Plots/gseaPlot_gsea_msigdbr_cat_",category_tag,"_",msig_GSEA_obj$ID[x],"_" ,name_de_res,".pdf"))
+ggsave(gsea_plot2, file = paste0("figures/",name_de_res,"/enrichment_Plots/GSEA_MSigDb_",category_tag,"/","gseaPlot_gsea_msigdbr_cat_",category_tag,"_",msig_GSEA_obj$ID[x],"_" ,name_de_res,".pdf"))
 
 }
 purrr::map(1:length(msig_GSEA_obj$ID), get_gsea_plot2)
@@ -425,6 +487,10 @@ ggsave(enrich_dist_plot_out, file = paste0("figures/",name_de_res,"/ridgePlot_gs
 
 message("extracting GSEA results table from GSEA object")
 msig_GSEA_results <- msig_GSEA_obj@result
+write.table(msig_GSEA_results, 
+file=paste0("data/",name_de_res,"/GSEA_msigdbr_cat_",category_tag,"_enrichedSets.tsv"),
+sep="\t", row.names = FALSE, col.names = TRUE)
+
 #rank gene sets by normalized  enrichment scores and q-values
 msig_GSEA_results_sig_qval <- msig_GSEA_results %>% dplyr::filter(qvalues < 0.05) %>% arrange(desc(abs(NES))) #%>% head() #sigGSEA
 print(head(msig_GSEA_results_sig_qval)) ##test results
@@ -442,7 +508,8 @@ coord_flip() +
 
 ggsave(lollipop_gsea, file = paste0("figures/",name_de_res,"/lollipop_gsea_msigdbr_cat_",category_tag,"_",name_de_res,"_top_frac_",nCategory_2show,".pdf"), width = ggwidth, height = ggheight)
 
-
+}
+}
 # #cnet plots
 # ## convert gene ID to Symbol
 # edox <- setReadable(msig_GSEA_obj, 'org.Hs.eg.db', 'ENTREZID')
@@ -453,23 +520,32 @@ ggsave(lollipop_gsea, file = paste0("figures/",name_de_res,"/lollipop_gsea_msigd
 # #cnetPlot_cowp <- cowplot::plot_grid(p1, p2, ncol=2, labels=LETTERS[1:2])
 # ggsave(p1, file = paste0("figures/cnetplot_gsea_msigdbr_cat_",category_tag,"_",name_de_res,".pdf"), width = ggwidth, height = ggheight)
 
+# gseaObj_name <- paste0("msig_category_",category_tag) #what name should be given to the gsea object in list
 
-#make list of results
-gseaObj_name <- paste0("msig_category_",category_tag) #what name should be given to the gsea object in list
-out_gsea_fun <- list(rankedFC=foldchanges,gseGO=gse, gseaObj_name = msig_GSEA_obj)
-saveRDS(out_gsea_fun, file=paste0("data/GSE_GO_plus_msigdbr_cat_",category_tag,"_",name_de_res,".rds"))
+# if(do_GSEA){
+# out_gsea_fun <- list(rankedFC=foldchanges, gseaObj_name = msig_GSEA_obj)
+# saveRDS(out_gsea_fun, file=paste0("data/GSEA_msigdbr_cat_",category_tag,"_",name_de_res,".rds"))
+# }else if(do_gse_GO){
+# out_gse <- list(rankedFC=foldchanges, gseGO=gse)
+# saveRDS(out_gse, file=paste0("data/GSE_GO_",name_de_res,".rds"))
+# }else(do_gse_GO & do_GSEA){
+#     message("saving GSE & GSEA enrichment distribution plots")
+#     #make list of results
+# out_gsea_fun <- list(rankedFC=foldchanges, gseGO=gse, gseaObj_name = msig_GSEA_obj)
+# saveRDS(out_gsea_fun, file=paste0("data/GSE_GO_plus_msigdbr_cat_",category_tag,"_",name_de_res,".rds"))
+# return(out_gsea_fun)
+# }
 
 # return(out_gsea_fun)
-return(out_gsea_fun)
-}
+#}
 
 #paste0("figures/gsea_msigdbr_H_type_CKI_DMSO_EnrichDistrib.pdf")
 #run gsea for all contrasts
 # lapply(names_contrasts_ls, function(x) gsea_analysis(x))
-h_gsea <- lapply(names_contrasts_ls, function(x) gsea_analysis(x, category_tag = "H", nCategory_2show = 100))
-c2_gsea <- lapply(names_contrasts_ls, function(x) gsea_analysis(x, category_tag = "C2", ggwidth = 15, ggheight = 12))
-names(h_gsea)
-names(h_gsea[[1]][["rankedFC"]])
+h_gsea <- lapply(contrasts_ls, function(x) gsea_analysis(x, category_tag = "H", nCategory_2show = 100, ggwidth = 15, ggheight = 12))
+c2_gsea <- lapply(contrasts_ls, function(x) gsea_analysis(x, do_gse_GO=FALSE,category_tag = "C2", ggwidth = 15, ggheight = 12))
+# names(h_gsea)
+# names(h_gsea[[1]][["rankedFC"]])
 
 # c2_gsea[[1]]
 # head(h_gsea[[1]]@result)
